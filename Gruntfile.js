7405ee6fef35218dfaf7d5fed2253b860f2b87eb
Craig Palermo <cpalermo2@gmail.com>
2015-05-24 14:14:57 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.initConfig({
+        /* for live reload, select which files to watch */
+        watch: {
+            livereload: {
+                files: ['coffee/**/*.coffee',
+                        'views/**/*.jade',
+                        'stylus/**/*.styl'],
+                options: { livereload: true },
+            },
+            coffee: {
+                files: ['coffee/**/*.coffee'],
+                tasks: ['coffee:compile']
+            },
+            jade: {
+                files: ['views/**/*.jade'],
+                tasks: ['jade:compile']
+            },
+            stylus: {
+                files: ['stylus/**/*.styl'],
+                tasks: ['stylus:compile']
+            }
+        },
+        /* select jade files to compile */
+        jade: {
+            compile: {
+                options: {
+                    pretty: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: "views/",
+                    src: ['**/partials/*.jade'],
+                    dest: './public/',
+                    ext: '.html'
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['jade'])
+};
